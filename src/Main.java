b058afd224a0c892246f7055ce82497a7ab64b12
Duda Sena <duda.sruiz@gmail.com>
2026-02-06 01:55:09 -0300

    feat: implement removeAt with element shifting

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+
+import listas.ListaSimples;
 
 public class Main {
+    public static void main(String[] args) {
+        // instanciando uma lista de Strings com capacidade para 3 nomes
+        ListaSimples<String> nomes = new ListaSimples<>(3);
+
+        System.out.println("--- Teste de Inserção ---");
+        nomes.insert("Duda");
+        nomes.insert("Maria");
+        nomes.insert("João");
+        System.out.println("Tamanho atual: " + nomes.size()); // Deve ser 3
+
+        System.out.println("\n--- Teste de Leitura (Get) ---");
+        System.out.println("Posição 0: " + nomes.get(0)); // Duda
+        System.out.println("Posição 1: " + nomes.get(1)); // Maria
+
+        System.out.println("\n--- Teste de Erro (Lista Cheia) ---");
+        try {
+            nomes.insert("Erro"); //isso deve disparar RuntimeException
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\n--- Teste de Erro (Índice Inválido) ---");
+        try {
+            System.out.println(nomes.get(10)); //índice inexistente
+        } catch (RuntimeException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("\n--- Teste de remoção ---");
+        nomes.removeAt(1);
+        System.out.println("Posição 1: " + nomes.get(1)); // agora é João, substituiu Maria
+
+    }
 }
--- src/listas/ListaSimples.java
+++ src/listas/ListaSimples.java
@@ -1,4 +1,54 @@
 package listas;
 
-public class ListaSimples {
+public class ListaSimples <T> {
+    private T[] elementos;
+    private int tamanho; //tamanho e posição são o mesmo número
+
+    @SuppressWarnings("unchecked") //para não alertar sobre transformar o Generics (Object) em "tipo T"
+    //garantir para o compilador que só entrarão itens do tipo T.
+    public ListaSimples(int capacidade) {
+        this.elementos = (T[]) new Object[capacidade];
+        this.tamanho = 0;
+    }
+
+    public boolean estaVazia() {
+        return tamanho == 0;
+    }
+
+    public boolean estaCheia() {
+        return tamanho == elementos.length;
+    }
+
+    public int size() {
+        return tamanho;
+    }
+
+    public void insert(T elemento) {
+        if (estaCheia()) {
+            throw new RuntimeException("A lista já atingiu o limite de capacidade!");
+        } //não precisa de else
+        elementos[tamanho] = elemento; //com os colchetes, aponta pra posição específica
+        tamanho++;
+    }
+
+    public T get(int indice) { //método pra retornar o elemento a partir da posição que o usuário pedir
+        if (indice < 0 || indice >= tamanho) {
+            throw new RuntimeException("O índice está fora do limite.");
+        }
+        return elementos[indice];
+    }
+
+    public T removeAt(int indice) {
+        if (indice < 0 || indice >= tamanho) {
+            throw new RuntimeException("O índice está fora do limite.");
+        }
+        T valorRemovido= elementos[indice];
+        for (int i = indice; i < tamanho -1; i++) {
+            elementos[i] = elementos[i + 1];
+        }
+        return valorRemovido;
+    }
 }
+
+
+
